04ba5fe455f303ca340cf05fa4f430aa0e633967
taotall <toatall@mail.ru>
2021-05-13 10:09:56 +0500

    Добавлены ссылки в подвал

--- views/layouts/portal.php
+++ views/layouts/portal.php
@@ -137,36 +137,49 @@ JS
 </div>
 
 <footer class="footer" style="margin-top: 20px; height: auto;">
-    <div class="container">
-        <p class="pull-left">
-            <b>Внутренние сайты и сервисы ФНС России:</b>
-            <br /><a href="http://portal.tax.nalog.ru" target="_blank">Портал ФНС России</a>
-            <br /><a href="http://support.tax.nalog.ru" target="_blank">Портал ФКУ "Налог-Сервис" ФНС России</a>
-            <br /><a href="https://support.gnivc.ru" target="_blank">Сайт технической поддержки АО "ГНИВЦ"</a>
-            <br /><a href="http://edu.tax.nalog.ru" target="_blank">Образовательный портал ФНС России</a>
-            <br /><a href="http://support.tax.nalog.ru/esk/phone/" target="_blank">Телефонный справочник работников ФНС / ФКУ</a>
-            <br /><a href="http://wiki.tax.nalog.ru/mw/index.php" target="_blank">Глоссарий ФНС России</a>
-            <br /><a href="http://riski.regions.tax.nalog.ru/autorize.html" target="_blank">Реестр рисков</a>
-            <br /><a href="http://lk3-usr.tax.nalog.ru/user/auth/index" target="_blank">Кабинет налогоплательщика юридического лица</a>
-            <br /><a href="https://rdmz-nlb-nginx.lkfl21.tax.nalog.ru/lkfl-ofc/login" target="_blank">Личный кабинет налогоплательщика — физического лица</a>
-            <br /><a href="http://ias.ais3.tax.nalog.ru/ais/" target="_blank">Программный комплекс информационно-аналитической работы</a>
-            <br /><a href="http://consultant.tax.nalog.ru" target="_blank">Консультант Плюс</a>
-            <br /><a href="https://n7701-koe606.dpc.tax.nalog.ru/" target="_blank">Экспедитор ОД</a>
-            <br /><a href="http://10.253.200.25:9082/#/rus/" target="_blank">Автоматизированная система контроля кассовой техники (АСК ККТ)</a>
-            <br /><a href="http://marmnpd.tax.nalog.ru:8081" target="_blank">Автоматизированное рабочее место «Налог на  профессиональный доход» (МАРМ НПД)</a>
-            <br /><a href="https://bo.dpc.tax.nalog.ru" target="_blank">Государственный информационный ресурс бухгалтерской (финансовой) отчетности (ГИР БО)</a>            
-        </p>
-        <p class="pull-right">
-            <b>Внутренние сервисы Управления:</b>
-            <br /><?= ''//Html::a('Рекомендуемые браузеры', array('site/browsers')); ?>
-            <br /><a href="http://u8600-app045:81" target="_blank">Реестр невзысканных сумм по налоговым проверкам (ВНП, КНП)</a>
-            <br /><a href="http://u8600-app045:82" target="_blank">Реестр прав доступа внешних ресурсов</a>
-            <br /><a href="http://u8600-app045:83" target="_blank">Электронный архив</a>
-            <br /><a href="http://u8600-app045:85" target="_blank">Реестр проверок органами государственного контроля и надзора</a>
-            <br /><a href="http://u8600-app045:86" target="_blank">Реестр МРГ</a>
-            <br /><a href="http://u8600-app036">Бывшие сотрудники</a>
-            <br /><a href="http://u8600-app012:88">Проект "Обращения"</a>
-        </p>
+    <div class="container-fluid" style="padding: 0 100px;">
+        <div class="row">
+            <div class="col-sm-4">
+                <h5><strong>Внутренние сайты и сервисы ФНС России</strong></h5>
+                <ul class="list-unstyled">
+                    <li><a href="http://portal.tax.nalog.ru" target="_blank">Портал ФНС России</a></li>
+                    <li><a href="http://support.tax.nalog.ru" target="_blank">Портал ФКУ "Налог-Сервис" ФНС России</a></li>
+                    <li><a href="https://support.gnivc.ru" target="_blank">Сайт технической поддержки АО "ГНИВЦ"</a></li>
+                    <li><a href="http://edu.tax.nalog.ru" target="_blank">Образовательный портал ФНС России</a></li>
+                    <li><a href="http://support.tax.nalog.ru/esk/phone/" target="_blank">Телефонный справочник работников ФНС / ФКУ</a></li>
+                    <li><a href="http://wiki.tax.nalog.ru/mw/index.php" target="_blank">Глоссарий ФНС России</a></li>
+                    <li><a href="http://riski.regions.tax.nalog.ru/autorize.html" target="_blank">Реестр рисков</a></li>
+                    <li><a href="http://lk3-usr.tax.nalog.ru/user/auth/index" target="_blank">Кабинет налогоплательщика юридического лица</a></li>
+                    <li><a href="https://rdmz-nlb-nginx.lkfl21.tax.nalog.ru/lkfl-ofc/login" target="_blank">Личный кабинет налогоплательщика — физического лица</a></li>                    
+                    <li><a href="http://consultant.tax.nalog.ru" target="_blank">Консультант Плюс</a></li>
+                </ul>                        
+            </div>
+            <div class="col-sm-4">
+                <h5><strong>Аналитическая подсистема АИС "Налог-3"</strong></h5>
+                <ul class="list-unstyled">
+                    <li><a href="http://ias.ais3.tax.nalog.ru/ais/" target="_blank">Программный комплекс информационно-аналитической работы</a></li>
+                    <li><a href="https://n7701-koe606.dpc.tax.nalog.ru/" target="_blank">Система управления запросами к озеру данных "Экспедитор"</a></li>
+                    <li><a href="http://10.253.200.25:9082/#/rus/" target="_blank">Автоматизированная система контроля кассовой техники (АСК ККТ)</a></li>
+                    <li><a href="http://marmnpd.tax.nalog.ru:8081" target="_blank">Автоматизированное рабочее место «Налог на  профессиональный доход» (МАРМ НПД)</a></li>
+                    <li><a href="https://bo.dpc.tax.nalog.ru" target="_blank">Государственный информационный ресурс бухгалтерской (финансовой) отчетности (ГИР БО)</a></li>
+                    <li><a href="http://vsa.ais3.tax.nalog.ru/vsaClient/vsa.jnlp" target="_blank">Прикладная подсистема «Визуальный сетевой анализ» (ПП ВСА)</a></li>
+                    <li><a href="http://ias.ais3.tax.nalog.ru/uprrep" target="_blank">Информационно-аналитическая подсистема "Управленческий учет"</a></li>
+                </ul>                
+            </div>
+            <div class="col-sm-4">
+                <h5><strong>Внутренние сервисы Управления</strong></h5>
+                <ul class="list-unstyled">
+                    <li><?= ''//Html::a('Рекомендуемые браузеры', array('site/browsers')); ?></li>
+                    <li><a href="http://u8600-app045:81" target="_blank">Реестр невзысканных сумм по налоговым проверкам (ВНП, КНП)</a></li>
+                    <li><a href="http://u8600-app045:82" target="_blank">Реестр прав доступа внешних ресурсов</a></li>
+                    <li><a href="http://u8600-app045:83" target="_blank">Электронный архив</a></li>
+                    <li><a href="http://u8600-app045:85" target="_blank">Реестр проверок органами государственного контроля и надзора</a></li>
+                    <li><a href="http://u8600-app045:86" target="_blank">Реестр МРГ</a></li>
+                    <li><a href="http://u8600-app036" target="_blank">Бывшие сотрудники</a></li>
+                    <li><a href="http://u8600-app012:88" target="_blank">Проект "Обращения"</a></li>
+                </ul>
+            </div>            
+        </div>                
     </div>
     <hr />
     <div class="container">
