c18976e07acbf3196b62b9a82a2139c522a6d3ab
taotall <toatall@mail.ru>
2021-11-19 12:25:00 +0500

    Добавлен скрипт по тестированию в шаблон приложения (для возможности внедрения тестов в новостях)

--- views/static/ib/_test.php
+++ views/static/ib/_test.php
@@ -2,8 +2,6 @@
 /* @var $this \yii\web\View */
 
 use yii\helpers\Url;
-use app\modules\test\assets\TestAsset;
-TestAsset::register($this);
 
 $tests = [
     // Материалы для государственных служащих
