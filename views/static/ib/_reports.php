fda5f058f16e9677220eb5457bd560c97da3d82c
taotall <toatall@mail.ru>
2021-12-24 14:24:36 +0500

    Обновление отчета по ИБ

--- views/static/ib/_reports.php
+++ views/static/ib/_reports.php
@@ -7,7 +7,7 @@
 
             <ul class="list-unstyled">
                 <li><a href="/files_static/8600/ib/reports/Перечень отчетов.xlsx" target="_blank">Перечень ежеквартальных отчетов</a></li>
-                <li><a href="/files_static/8600/ib/reports/регламентирующие документы.zip" target="_blank">Регламентирующие документы</a></li>        
+                <li><a href="/files_static/8600/ib/reports/регламентирующие документы (new).zip" target="_blank">Регламентирующие документы</a></li>        
             </ul>    
         
         </div>
