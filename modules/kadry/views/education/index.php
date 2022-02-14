3cbda6f10d9a32afa55bf2f3bb182bce97e4b9aa
taotall <toatall@mail.ru>
2022-02-14 16:58:21 +0500

    Исправлена проблема отображения изображения в IE

--- modules/kadry/views/education/index.php
+++ modules/kadry/views/education/index.php
@@ -17,7 +17,7 @@ $this->title = 'Комплекс программ профессионально
 <?php foreach ($models as $item): ?>
     <div class="col-md-4">
         <div class="card mb-4 shadow-sm">
-            <img src="<?= $item->getThumbnailImage() ?>" />
+            <img src="<?= $item->getThumbnailImage() ?>" style="height: 100%; width: 100%;" />
             <hr class="m-0" />           
             <div class="card-header">
                 <h5 class="font-weight-bolder"><?= $item->title ?? null ?></h5>
