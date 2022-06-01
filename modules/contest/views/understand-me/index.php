75961a471bdd096ff007b0922cc16f9e5a52431e
taotall <toatall@mail.ru>
2022-06-01 13:54:43 +0500

    Изменено отображение фотографий по конкурсу "Пойми меня"

--- modules/contest/views/understand-me/index.php
+++ modules/contest/views/understand-me/index.php
@@ -13,15 +13,18 @@ $this->title = 'Пойми меня';
     <?php       
         foreach($data->data as $item): ?>
         
-        <div class="col-4 d-flex align-items-stretch mb-3">
+        <div class="col-4 d-flex align-items-stretch mb-5">
             <a href="<?= Url::to(['/contest/understand-me/view', 'id'=>$item->id]) ?>" class="mv-link">
-                <div class="card text-white h-100 shadow" style="background-color: darkgrey;">                
-                    <img src="<?= $item->image ?>" class="card-img" />
+                <div class="card text-white h-100" style="background: none; border-width: 0;">                
+                    <img src="<?= $item->image ?>" class="card-img" />                   
                     <div class="card-img-overlay">
                         <div class="row align-items-end h-100 w-100">
-                            <span class="font-weight-bolder fa-2x ml-4" style="text-shadow: 2px 2px #555;">    
-                                <?= $item->title ?>
-                            </span>
+                            <div class="col text-center">
+                                <span class="font-weight-bolder fa-2x" style="text-shadow: 2px 2px #555, -1px -1px #888, 1px 3px #444, 3px 1px #777;">    
+                                    <?= $item->title ?>
+                                </span>
+                            </div>
+                            
                         </div>
                     </div>                
                 </div>
