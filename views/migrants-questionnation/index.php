21ea93ea9783542c44d186f76b50d40a166b65f9
taotall <toatall@mail.ru>
2022-11-08 15:47:11 +0500

    +

--- views/migrants-questionnation/index.php
+++ views/migrants-questionnation/index.php
@@ -33,11 +33,25 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
             //'id',
-            'ul_name',
+            [
+                'attribute' => 'ul_name',
+                'label' => $searchModel->getAttributeLabel('ul_name') . ' <i class="fas fa-info-circle" data-bs-toggle="tooltip" data-bs-title="Наименование ЮЛ, представивших в ЕЦР (86038) заявления о предстоящем изменении адреса места нахождения (в иной субъект РФ)"></i>',                
+                'encodeLabel' => false,
+            ],            
             'ul_inn',
-            'ul_kpp',
-            'date_send_notice:date',
+            'ul_kpp',            
+            [
+                'attribute' => 'date_send_notice',
+                'label' => $searchModel->getAttributeLabel('date_send_notice') . ' <i class="fas fa-info-circle" data-bs-toggle="tooltip" data-bs-title="Дата направления уведомления о смене адреса в регистрирующий орган"></i>',
+                'encodeLabel' => false,
+                'format' => 'date',
+            ],
             'region_migrate',
+            [
+                'attribute' => 'region_migrate',
+                'label' => $searchModel->getAttributeLabel('region_migrate') . ' <i class="fas fa-info-circle" data-bs-toggle="tooltip" data-bs-title="Регион РФ, в который планирует мигрировать ЮЛ"></i>',
+                'encodeLabel' => false,
+            ],
             'cause_migrate',
             //'date_create',
             //'date_update',
@@ -65,6 +79,8 @@ $this->params['breadcrumbs'][] = $this->title;
         $.pjax.reload({ container: '#pjax-migrants-questionnation-index'});        
     });
     
+    $('.migrants-questionnation-index [data-bs-toggle="tooltip"]').tooltip();
+
     JS); ?>
     <?php Pjax::end(); ?>
 
