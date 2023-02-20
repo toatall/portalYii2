8b76bad2d7ff91e7e17df207d2a5a4a4e2429f0d
taotall <toatall@mail.ru>
2023-02-20 09:45:54 +0500

    Изменен реестр ПО

--- views/static/ib/_reestrPO.php
+++ views/static/ib/_reestrPO.php
@@ -6,9 +6,9 @@
 <div class="card card-body">
     <ul class="list-unstyled">
         <li>
-            <a href="/files_static/8600/ib/reestr_po/Реестр ПО 05072022.pdf" target="_blank">
+            <a href="/files_static/8600/ib/reestr_po/Реестр разрешенного ПО.pdf" target="_blank">
                 Реестр разрешенного программного обеспечения
-                <br /><span class="badge bg-secondary">от 05.07.2022</span>
+                <br /><span class="badge bg-secondary">от 16.02.2023</span>
             </a>
         </li>      
     </ul>    
