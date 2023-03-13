842f1c5b067df16c1448819e64af4be7a6f5253a
taotall <toatall@mail.ru>
2023-03-13 11:42:34 +0500

    Добавлен js-скрипт открытия tabs и accordeon на основании хэша в ссылке

--- views/static/ib/ib.php
+++ views/static/ib/ib.php
@@ -14,28 +14,33 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 
 <?= Tabs::widget([
-    'id' => 'tab_id_index',
+    'id' => 'ib',
     'items' => [
         [
             'label' => 'Нормативные документы',
             'content' => $this->render('_normative'),
             'active' => true,
+            'options' => ['id' => 'normative-docs'],
         ],
         [
             'label'=>'Заявки',
             'content'=>$this->render('_zayavki'),
+            'options' => ['id' => 'zayavki'],
         ],
         [
             'label'=>'Тестирование',
             'content'=>$this->render('_test'),
+            'options' => ['id' => 'tests'],
         ],
         [
             'label'=>'Ежеквартальные отчеты',
             'content'=>$this->render('_reports'),
+            'options' => ['id' => 'reports'],
         ],
         [
             'label'=>'Реестр разрешенного программного обеспечения',
             'content'=>$this->render('_reestrPO'),
+            'options' => ['id' => 'reestr-po'],
         ],
     ],
     'options' => [
@@ -44,5 +49,29 @@ $this->params['breadcrumbs'][] = $this->title;
 ]);
 
 $this->registerJs(<<<JS
-    $('.accordion .show').collapse('hide');
+    
+   $('.accordion .show').collapse('hide');
+    
+    function openTabById(params) {
+        let parts = params.split('=');
+        $('#' + parts[0] + ' a[href="#' + parts[1] + '"]').tab('show');
+    }
+        
+    function openCollapseById(param) {        
+        $('#' + param).collapse('show');
+        console.log('#' + param);
+    }
+        
+    let hash = window.location.hash;
+    if (hash.match('#')) {
+        let hashData = hash.split('#')[1];
+        let parts = hashData.split('&');
+        if (parts[0] != null) {
+            openTabById(parts[0]);
+        }
+        if (parts[1] != null) {
+            openCollapseById(parts[1]);
+        }
+    }
+        
 JS); ?>
