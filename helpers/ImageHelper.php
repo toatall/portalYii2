e1f0ae8df65df16e2389e64c69eb59f4d59ab432
taotall <toatall@mail.ru>
2023-03-16 11:52:46 +0500

    Новый хелпер по поиску изображений

--- helpers/ImageHelper.php
+++ helpers/ImageHelper.php
@@ -3,19 +3,26 @@ namespace app\helpers;
 
 use yii\helpers\FileHelper as HelpersFileHelper;
 
-
+/**
+ * Поиск только файлов-изображений
+ * png|jpg|jpeg|gif|bmp|svg|jfif|webp
+ * @author toatall
+ */
 class ImageHelper extends HelpersFileHelper
 {
 
     /**
      * Returns the images found under the specified directory and subdirectories.
+     * @param string $dir Full path
+     * @param array $options 
      * @return array
      */
     public static function findImages($dir, $options = [])
     {
         return parent::findFiles($dir, array_merge([
-            'filter' => function(string $path):bool {
-                return preg_match('/((\.png)|(\.jpg)|(\.jpeg)|(\.gif)|(\.bmp)|(\.svg)|(\.jfif)|(\.webp))$/i', $path);
+            'filter' => function(string $filename):bool {               
+                $mimeType = \yii\helpers\FileHelper::getMimeType($filename);
+                return substr($mimeType, 0, 5) === 'image';
             },
         ], $options));
     }
