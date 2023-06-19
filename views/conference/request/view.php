13d5ba0976d9bee47d3e4d718e1cabec8847c095
taotall <toatall@mail.ru>
2023-06-19 11:05:41 +0500

    Удаление раздела conference

--- views/conference/request/view.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** @var yii\web\View $this */
-/** @var app\models\conference\Conference $model */
-
-?>
-
-<?= $this->render('_statuses', ['model' => $model]) ?>
-
-<?= $this->render('../view', ['model' => $model]) ?>
