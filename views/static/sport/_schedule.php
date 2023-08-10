5e229bd6fe86ae430d1e7b3c15b8e7290feea34e
taotall <toatall@mail.ru>
2023-08-10 16:43:40 +0500

    **

--- views/static/sport/_schedule.php
+++ views/static/sport/_schedule.php
@@ -2,11 +2,11 @@
 
 /** @var \yii\web\View $this */
 
-$file = '/files_static/8600/sport/schedule.pdf';
+$file = '/files_static/8600/sport/schedule111.pdf';
 ?>
 
 <object data="<?= $file ?>" style="width: 100%; height: 100vh;" type="application/pdf">
     <embed data="<?= $file ?>" type="application/pdf"></embed>
     <p>Ваш браузер не поддерживает просмотр pdf.</p>
-    <a href="<?= $file . "?" . time() ?>">Скачать</a>
+    <a href="<?= $file ?>">Скачать</a>
 </object>
