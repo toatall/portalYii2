2f0aea6a709233a2d055d9890ab69b8cc8d3c81a
taotall <toatall@mail.ru>
2023-10-10 16:40:37 +0500

    + автоматизация рутины

---
+++ models/AutomationRoutine.php
@@ -0,0 +1,285 @@
+<?php
+
+namespace app\models;
+
+use app\behaviors\AuthorBehavior;
+use app\behaviors\DatetimeBehavior;
+use app\behaviors\FileUploadBehavior;
+use Yii;
+use yii\db\Expression;
+use yii\helpers\FileHelper;
+
+/**
+ * This is the model class for table "{{%automation_routine}}".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $description
+ * @property string $owners
+ * @property string|null $ftp_path
+ * @property string $author
+ * @property string $date_create
+ * @property string|null $date_update
+ * 
+ * @property string $uploadPath
+ *
+ */
+class AutomationRoutine extends \yii\db\ActiveRecord
+{
+
+    /**
+     * Файлы ПМ
+     * @var string[]
+     */
+    public $uploadFiles;
+
+    /**
+     * Имена файлов для удаления
+     * @var mixed[]
+     */
+    public $deleteFiles;
+
+    /**
+     * @var mixed
+     */
+    public $uploadInstruction;
+
+    /**
+     * @var bool
+     */
+    public $deleteInstruction;
+
+    /** 
+     * Переопределение из FileUploadBehavior
+     */
+    public $makeThumbs = false;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%automation_routine}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'owners'], 'required'],
+            [['description'], 'string'],
+            [['date_create', 'date_update'], 'safe'],
+            [['title'], 'string', 'max' => 300],
+            [['ftp_path', 'author'], 'string', 'max' => 250],
+            [['uploadFiles'], 'file', 'skipOnEmpty' => true, 'maxFiles' => 30],            
+            [['uploadInstruction'], 'file', 'skipOnEmpty' => true],
+            [['deleteFiles', 'deleteInstruction'], 'safe'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        return [            
+            [
+                'class' => DatetimeBehavior::class,
+                'createdAtAttribute' => 'date_create',
+                'updatedAtAttribute' => 'date_update',
+            ],
+            ['class' => AuthorBehavior::class],
+            ['class' => FileUploadBehavior::class],
+        ];
+    }
+    
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ИД',
+            'title' => 'Заголовок',
+            'description' => 'Описание',           
+            'ftp_path' => 'Ссылка на ftp',
+            'owners' => 'Владелец (структурное подразделение)',
+            'author' => 'Автор',
+            'date_create' => 'Дата создания',
+            'date_update' => 'Дата изменения',
+            'uploadFiles' => 'Файлы ПМ',
+            'uploadInstruction' => 'Инструкция',
+            'deleteFiles' => 'Удалить файлы',
+            'deleteInstruction' => 'Удалить инструкцию',
+        ];
+    }
+
+    /**
+     * Сохранение информации о скачанном файле
+     * @return int
+     */
+    public function saveDownload($filename)
+    {
+        return Yii::$app->db->createCommand()
+            ->insert('{{%automation_routine_downloads}}', [
+                'id_automation_routine' => $this->id,
+                'filename' => $filename,
+                'author' => Yii::$app->user->identity->username,
+                'date_create' => new Expression('getdate()'),
+            ])
+            ->execute();
+    }
+
+    /**
+     * Базовый каталог
+     * @return string
+     */
+    private function getBasePath($id)
+    {
+        return "/public/upload/portal/automation-routine/{$id}/";
+    }
+
+    /**
+     * Каталог с файлами
+     * @return string
+     */
+    private function getFilesPath()
+    {
+        return $this->getBasePath($this->id) . "files/";
+    }
+
+    /**
+     * Каталог с инструкцией
+     * @return string
+     */
+    private function getInstructionPath()
+    {
+        return $this->getBasePath($this->id);
+    }
+
+    /**
+     * Поиск файлов
+     * @return string[]|null
+     */
+    private function searchFiles($path)
+    {
+        if (is_dir($path)) {
+            return FileHelper::findFiles($path, ['recursive' => false]);
+        } 
+    }
+    /**
+     * Подготовка имен файлов
+     * @param string
+     * @return string[]
+     */
+    private function prepareFiles($path)
+    {
+        $files = $this->searchFiles(Yii::getAlias('@webroot') . $path);
+        $result = [];
+        if ($files && is_array($files) && count($files)) {
+            foreach($files as $file) {
+                $result[] = $path . basename($file);
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getFiles()
+    {
+        return $this->prepareFiles($this->getFilesPath());
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getInstruction()
+    {
+        return $this->prepareFiles($this->getInstructionPath())[0] ?? null;
+    }
+
+    /**
+     * Загрузка файлов
+     */
+    protected function uploadingFiles(): void
+    {
+        // если есть что удалять, то сначала удаляем
+        if ($this->deleteFiles) {
+            $this->deleteFiles($this->deleteFiles);           
+        }
+
+        $this->uploadPath = $this->getFilesPath();
+        $this->uploadFiles('uploadFiles');
+    }
+
+    /**
+     * Загрузка инструкции
+     */
+    protected function uploadingInstruction()
+    {        
+        $instrunction = $this->getInstruction();
+        if ($this->deleteInstruction) {
+            if ($instrunction) { 
+                @FileHelper::unlink(Yii::getAlias('@webroot') . $instrunction); 
+            }
+        }
+
+        $this->uploadPath = $this->getInstructionPath();
+        $this->uploadFiles('uploadInstruction', function() use ($instrunction) { 
+            if ($instrunction) { 
+                @FileHelper::unlink(Yii::getAlias('@webroot') . $instrunction); 
+            } 
+        });
+    }
+
+    /**
+     * @param string[]|null $files файлы отмеченны для удаления  
+     */
+    private function deleteFiles($files) 
+    {               
+        if (!$files) {
+            return;
+        }
+        $files = array_filter((array)$files);
+        
+        foreach ($files as $file) {           
+            FileHelper::unlink(Yii::getAlias('@webroot') . $file);            
+        }
+    }
+
+    /**
+     * Загрузка всех файлов
+     */
+    protected function uploadAll()
+    {   
+        $this->uploadingFiles();
+        $this->uploadingInstruction();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+        $this->uploadAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function afterDelete()
+    {
+        parent::afterDelete();
+        $this->deleteFiles($this->getFiles());
+        $this->deleteFiles($this->getInstruction());
+    }
+   
+
+}
---
+++ models/AutomationRoutineSearch.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\models;
+
+use yii\data\ActiveDataProvider;
+use app\models\AutomationRoutine;
+
+/**
+ * AutomationRoutineSearch represents the model behind the search form of `app\models\AutomationRoutine`.
+ */
+class AutomationRoutineSearch extends AutomationRoutine
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['title', 'description', 'ftp_path', 'author', 'date_create', 'date_update', 'owners'], 'safe'],
+        ];
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = AutomationRoutine::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'date_create' => $this->date_create,
+            'date_update' => $this->date_update,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'ftp_path', $this->ftp_path])
+            ->andFilterWhere(['like', 'owners', $this->owners]);
+            // ->andFilterWhere(['like', 'author', $this->author]);
+
+        return $dataProvider;
+    }
+}
